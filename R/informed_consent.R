4ef4f3784a67761b99a989daa72fb5e4a0e4a48e
the-tave <you@example.comoverland@uni-bremen.de>
2025-01-20 19:26:58 +0100

    added informed_consent function

---
+++ R/informed_consent.R
@@ -0,0 +1,17 @@
+#' Informed Consent Filter
+#' 
+#' One of the best practices in data collection is including and informed consent question.
+#' With this function you can automatically filter out rows where participants did not explicitly consent to their data being used.
+#'
+#' @param dataframe A dataframe of data collected with WEXTOR
+#' @param varname character. Variable name of informed consent variable. Defaults to "informedconsent".
+#'
+#' @returns Returns data with cases filtered out that did not check the informed constent box
+#' @export
+#'
+#' @examples
+informed_constent <- function(dataframe, varname = "informedconsent"){
+  
+  new <- dataframe[dataframe[varname] == "checked", ] |> na.omit()
+  new
+}
